4e0c0e93d3548959afee0d6772986d8f1b80810e
Torben Korb <korb@a-w-media.de>
2015-11-30 02:10:03 +0100

    Added counters and functionality to make the app work

--- app.js
+++ app.js
@@ -8,7 +8,9 @@
 
 'use strict';
 
-// 1.) Create an array with the unique items. For example some nouns
+
+
+// Create an array with the unique items.
 var subjects = [
   'House',
   'Mouse',
@@ -19,12 +21,32 @@ var subjects = [
 ];
 
 
+// Create Variables that will be used later
+
+var shownSubject;
+var compareSubject;
+var points = 0;
+var pointsCounter = document.getElementById('counter');
+var attempts = 0;
+var attemptsCounter = document.getElementById('attemptsCounter');
+
+
+// Print points variable to the browser
+function printCounters(points, attempts) {
+  pointsCounter.innerText=points;
+  attemptsCounter.innerText=attempts;
+}
+printCounters(points, attempts);
+
+
+
 // 2.) Double the items in the array and append it to the array
 var memoryBoard = subjects.slice(0);
 
 for ( var i = 0; i < subjects.length; i++ ) {
   memoryBoard.push(subjects[i]);
 }
+
 
 
 // 3.) Shuffle the items from the array and print them to the browser. CSS hides content, by click it will be shown
@@ -34,43 +56,43 @@ for ( var i = 0; i < memoryBoard.length; i++ ) {
   document.getElementById('memorycard-list').innerHTML += '<li><p>' + memoryBoard[i] + '</div></p>';
 }
 
-console.log(memoryBoard);
 
-var shownSubject;
-var compareSubject;
-var points = 0;
 
-document.getElementById('counter').innerText=points;
+
+
+
+
 
 // Create click event
 $('p').click(function() {
 
   $(this).addClass('open');
 
-  if( $('.open').length <= 1) {
-    shownSubject = $(this).text();
+  var openCards = $('.open');
+
+  if( openCards.length <= 1) {
+    shownSubject = $(this);
   }
 
-  if( $('.open').length >= 2) {
-    compareSubject = $(this).text();
+  if( openCards.length >= 2) {
+    compareSubject = $(this);
+
+    if( shownSubject.text() !== compareSubject.text() ) {
+      attempts++;
+      alert("Fail...");
+      shownSubject.removeClass('open');
+      compareSubject.removeClass('open');
+    } else {
+      points+=100;
+      attempts++;
+      alert("GOAL!!!!!");
+      shownSubject.removeClass('open').addClass('matched');
+      compareSubject.removeClass('open').addClass('matched');
+    }
+
   }
 
-  console.log(shownSubject, compareSubject);
-
-  if( shownSubject !== compareSubject ) {
-    console.log("Fail...");
-  } else {
-    console.log("GOAL!!!!!");
-    points+=100;
-  }
-
-  document.getElementById('counter').innerText=points;
-
-
-  if ( $('.open').length >= 2 ) {
-    console.log('No more!');
-  }
-
+  printCounters(points, attempts);
   
 })
 
